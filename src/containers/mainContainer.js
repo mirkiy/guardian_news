9064411dec39423a9f9f3457e0a9454c164a4f13
mirkiy <mirizachar@gmail.com>
2023-05-18 15:28:51 +0100

    fetch data from api

--- src/containers/mainContainer.js
+++ src/containers/mainContainer.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const MainContainer = () => {
-  return <h2> hello from the main container</h2>;
+  useEffect(() => {
+    getArticles();
+  }, []);
+  const getArticles = () => {
+    fetch(
+      "https://content.guardianapis.com/search?q=brexit&format=json&api-key=test"
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
+  return (
+    <>
+      <h2> hello from the main container</h2>
+    </>
+  );
 };
 
 export default MainContainer;
